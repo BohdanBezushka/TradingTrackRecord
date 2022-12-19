9b508195d9bd0647ef1c8beff1407c4fc32b5e63
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2022-12-19 20:05:38 +0000

    Access the TradingTrackRecord file from google sheet

--- run.py
+++ run.py
@@ -4,3 +4,20 @@
 
 import gspread
 from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+TRADINGTRACKRECORD = Credentials.from_service_account_file("tradingtrackrecord.json")
+SCOPED_TRADINGTRACKRECORD = TRADINGTRACKRECORD.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_TRADINGTRACKRECORD)
+SHEET = GSPREAD_CLIENT.open("TradingTrackRecord")
+
+record = SHEET.worksheet("Year 2023")
+
+data = record.get_all_values()
+
+print(data)
