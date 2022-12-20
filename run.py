f546c2e0603557d2fad32ab1959b41e528392b2c
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2022-12-20 02:27:25 +0000

    Create update_date_worksheet function to add date in worksheet

--- run.py
+++ run.py
@@ -17,9 +17,9 @@ SCOPED_TRADINGTRACKRECORD = TRADINGTRACKRECORD.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_TRADINGTRACKRECORD)
 SHEET = GSPREAD_CLIENT.open("TradingTrackRecord")
 
-"""
-record = SHEET.worksheet("Year 2023")
 
+
+"""
 data = record.get_all_values()
 
 print(data)
@@ -35,6 +35,9 @@ def get_date():
     print("Two-digit day, two-digit month, Four-digit year  (example: 11-03-2023).")
 
     while True:
+        """
+        The user has to enter an exact date if not the loop will repeatedly request data, until it is valid.
+        """
         try:
             day_session = input("Enter date:")
             datetime.strptime(day_session,'%d-%m-%Y')
@@ -42,5 +45,19 @@ def get_date():
             break
         except ValueError:
             print("The date entered is incorrect")
+    return day_session
 
-get_date()
+def update_date_worksheet(date):
+    """
+    Update the worksheet, add the date specified by the user in the "Date" column. 
+    """
+    print("Adding date to the TradingTrackRecord worksheet")
+    date_worksheet = SHEET.worksheet("Year 2023")
+    date_worksheet.append_row(date)
+    print("Sales worksheet updated successfully.\n")
+
+
+
+date = get_date()
+update_date_worksheet(date)
+print(date)
